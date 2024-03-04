4446b1befbbe197ab7f3069040e881be5b970b68
student-shoaibsoomro <159219257+student-shoaibsoomro@users.noreply.github.com>
2024-03-05 04:53:48 +0500

    TypeScript_Practice
    My 26-02-2024 Practice

---
+++ personal_message.js
@@ -0,0 +1,4 @@
+"use strict";
+let Personal_Message = 'Hello Eric,';
+let message = 'would you like to learn some Python today?';
+console.log(Personal_Message + message);
---
+++ personal_message.ts
@@ -0,0 +1,4 @@
+let Personal_Message :string = 'Hello Eric,';
+let message :string = 'would you like to learn some Python today?';
+
+console.log(Personal_Message + message)
